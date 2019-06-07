d8cddb6ad314cbc5172de67c94e993a55c810e1a
tomomik9 <tomomi.kanaz@gmail.com>
2019-06-07 20:02:07 +0900

    first commit(コメント)

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,5 @@
-class Book < ApplicationRecord
+class Report < ApplicationRecord
   has_many :comments, as: :commentable
-  mount_uploader :picture, PictureUploader
   validates :title, presence: true, uniqueness: true
   validates :memo,  presence: true
-  validates :author, presence: true
 end
