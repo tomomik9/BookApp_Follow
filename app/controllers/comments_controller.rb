431dde609b38566b6747c744dc44eef2219a0965
tomomik9 <tomomi.kanaz@gmail.com>
2019-06-10 16:07:55 +0900

    comments_controller.rb編集および.gitignore編集済み

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,59 +1,46 @@
 class CommentsController < ApplicationController
+  before_action :find_comments, only: [:edit, :destroy, :update]
   def create
-    if params[:report_id]
-      @report = Report.find(params[:report_id])
-      @comment = @report.comments.create(comment_params)
-      @comment.user_id = current_user.id
-      @comment.save
-      redirect_to report_path(@report)
-    elsif params[:book_id]
-      @book = Book.find(params[:book_id])
-      @comment = @book.comments.create(comment_params)
-      @comment.user_id = current_user.id
-      @comment.save
-      redirect_to book_path(@book)
-    end
+    @comment = set_params.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to set_path
   end
 
   def destroy
-    if params[:report_id]
-      @report = Report.find(params[:report_id])
-      @comment = @report.comments.find(params[:id])
-      @comment.destroy
-      redirect_to report_path(@report)
-    else
-      @book = Book.find(params[:book_id])
-      @comment = @book.comments.find(params[:id])
-      @comment.destroy
-      redirect_to book_path(@book)
-    end
+    @comment.destroy
+    redirect_to set_path
   end
 
   def edit
-    if params[:report_id]
-      @report = Report.find(params[:report_id])
-      @comment = @report.comments.find(params[:id])
-    else
-      @book = Book.find(params[:book_id])
-      @comment = @book.comments.find(params[:id])
-    end
   end
 
   def update
-    if params[:report_id]
-      @report = Report.find(params[:report_id])
-      @comment = @report.comments.find(params[:id])
-      @comment.update(comment_params)
-      redirect_to report_path(@report)
-    else
-      @book = Book.find(params[:book_id])
-      @comment = @book.comments.find(params[:id])
-      @comment.update(comment_params)
-      redirect_to book_path(@book)
-    end
+    @comment.update(comment_params)
+    redirect_to set_path
   end
 
   private
+  def set_params
+    if params[:report_id]
+      Report.find(params[:report_id])
+    elsif params[:book_id]
+      Book.find(params[:book_id])
+    end
+  end
+
+  def set_path
+    if params[:report_id]
+      report_path(set_params)
+    elsif params[:book_id]
+      book_path(set_params)
+    end
+  end
+
+  def find_comments
+    @comment = set_params.comments.find(params[:id])
+  end
+
   def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
