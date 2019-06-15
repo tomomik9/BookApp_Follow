0343054fe8ab5007249426c1b658eb8b4d8d36d1
tomomik9 <tomomi.kanaz@gmail.com>
2019-06-16 06:07:32 +0900

    deviseの課題変更に伴い、カラムを3種類追加(郵便番号、住所、自己紹介文)、またUserのコントローラ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_093556) do
+ActiveRecord::Schema.define(version: 2019_06_15_210142) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 2019_06_07_093556) do
     t.string "provider"
     t.string "username"
     t.string "avatar_url"
+    t.string "postal_code"
+    t.string "address"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
