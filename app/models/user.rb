91195b111b9ffd8ae03d5a9b3cce8ab9bfdb3b39
tomomik9 <tomomi.kanaz@gmail.com>
2019-06-16 19:18:34 +0900

    app/models以下のファイルとバリデーション最終確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,14 @@ class User < ApplicationRecord
   has_many :following, through: :active_friendships,  source: :followed
   has_many :followers, through: :passive_friendships, source: :follower
 
-  # ユーザーをフォローする
   def follow(other_user)
     following << other_user
   end
 
-  # ユーザーをフォロー解除する
   def unfollow(other_user)
     active_friendships.find_by(followed_id: other_user.id).destroy
   end
 
-  # 現在のユーザーがフォローしてたらtrueを返す
   def following?(other_user)
     following.include?(other_user)
   end
