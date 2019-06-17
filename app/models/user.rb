9d4cfd193d67c4c04048c9d73e48f9a2419242a3
tomomik9 <tomomi.kanaz@gmail.com>
2019-06-17 20:01:16 +0900

    app/models/user.rbのactive_relationshipの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, 
          omniauth_providers: %i(github)
+
   has_many :active_friendships,  class_name: "Friendship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_friendships, class_name: "Friendship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_friendships,  source: :followed
